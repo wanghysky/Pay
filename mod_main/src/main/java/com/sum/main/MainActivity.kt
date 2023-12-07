a2cbc34a3ed378bfd85e6d5167829a0e1b1f031d
453560478@qq.com <why>
2023-12-07 17:57:05 +0800

    main

---
+++ mod_main/src/main/java/com/sum/main/MainActivity.kt
@@ -0,0 +1,88 @@
+package com.sum.main
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.alibaba.android.arouter.facade.annotation.Route
+import com.sum.common.constant.KEY_INDEX
+import com.sum.common.constant.MAIN_ACTIVITY_HOME
+import com.sum.framework.base.BaseDataBindActivity
+import com.sum.framework.log.LogUtil
+import com.sum.framework.toast.TipsToast
+import com.sum.framework.utils.AppExit
+import com.sum.framework.utils.StatusBarSettingHelper
+import com.sum.main.databinding.ActivityMainBinding
+import com.sum.main.navigator.SumFragmentNavigator
+
+/**
+ * @author mingyan.su
+ * @time   2023/3/3 8:41
+ * @desc   主页
+ */
+@Route(path = MAIN_ACTIVITY_HOME)
+class MainActivity : BaseDataBindActivity<ActivityMainBinding>() {
+
+    private lateinit var navController: NavController
+
+    companion object {
+        fun start(context: Context, index: Int = 0) {
+            val intent = Intent(context, MainActivity::class.java)
+            intent.putExtra(KEY_INDEX, index)
+            context.startActivity(intent)
+        }
+    }
+
+    override fun initView(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme)
+        val navView = mBinding.navView
+        //1.寻找出路由控制器对象，它是路由跳转的唯一入口，找到宿主NavHostFragment
+        navController = findNavController(R.id.nav_host_fragment_activity_main)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
+        //2.自定义FragmentNavigator，mobile_navigation.xml文件中的fragment标识改为SumFragmentNavigator的sumFragment
+        val fragmentNavigator = SumFragmentNavigator(this, navHostFragment.childFragmentManager, navHostFragment.id)
+        //3.注册到Navigator里面，这样才找得到
+        navController.navigatorProvider.addNavigator(fragmentNavigator)
+        //4.设置Graph，需要将activity_main.xml文件中的app:navGraph="@navigation/mobile_navigation"移除
+        navController.setGraph(R.navigation.mobile_navigation)
+        //5.将NavController和BottomNavigationView绑定，形成联动效果
+        navView.setupWithNavController(navController)
+        StatusBarSettingHelper.setStatusBarTranslucent(this)
+        StatusBarSettingHelper.statusBarLightMode(this@MainActivity, true)
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        intent?.let {
+            val index = intent.getIntExtra(KEY_INDEX, 0)
+//            navController.navigate(R.id.navi_home)
+            LogUtil.e("onNewIntent:index:$index", tag = "smy")
+        }
+    }
+
+    //延迟初始化执行的任务 此处的时机应该是在页面渲染首帧后执行 这里暂时放在onWindowFocusChanged()
+    //利用 IDLEHandler 特性，主线程空闲时机执行
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        //延迟执行启动器
+//        DelayInitDispatcher().addTask(InitTaskA())
+//                .addTask(InitTaskB())
+//                .start()
+    }
+
+    override fun onBackPressed() {
+//        if (mCurFragment?.onBackPressed() == true) {
+//            return
+//        }
+        AppExit.onBackPressed(
+            this,
+            { TipsToast.showTips(getString(R.string.app_exit_one_more_press)) }
+        )
+    }
+
+
+}
---
+++ mod_main/src/main/java/com/sum/main/ui/mine/MineFragment.kt
@@ -0,0 +1,246 @@
+package com.sum.main.ui.mine
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.alibaba.android.arouter.launcher.ARouter
+import com.scwang.smart.refresh.layout.api.RefreshLayout
+import com.scwang.smart.refresh.layout.listener.OnLoadMoreListener
+import com.scwang.smart.refresh.layout.listener.OnRefreshListener
+import com.sum.common.constant.DEMO_ACTIVITY_LIFECYCLE
+import com.sum.common.constant.DEMO_ACTIVITY_LIVEDATA
+import com.sum.common.constant.DEMO_ACTIVITY_NAVIGATION
+import com.sum.common.constant.DEMO_ACTIVITY_VIEWMODEL
+import com.sum.common.constant.USER_ACTIVITY_COLLECTION
+import com.sum.common.constant.USER_ACTIVITY_INFO
+import com.sum.common.constant.USER_ACTIVITY_SETTING
+import com.sum.common.model.User
+import com.sum.common.provider.LoginServiceProvider
+import com.sum.common.provider.MainServiceProvider
+import com.sum.common.provider.UserServiceProvider
+import com.sum.framework.base.BaseMvvmFragment
+import com.sum.framework.decoration.NormalItemDecoration
+import com.sum.framework.ext.gone
+import com.sum.framework.ext.onClick
+import com.sum.framework.ext.visible
+import com.sum.framework.log.LogUtil
+import com.sum.framework.toast.TipsToast
+import com.sum.framework.utils.dpToPx
+import com.sum.framework.utils.getStringFromResource
+import com.sum.glide.loadFile
+import com.sum.main.R
+import com.sum.main.databinding.FragmentMineBinding
+import com.sum.main.databinding.FragmentMineHeadBinding
+import com.sum.main.ui.mine.viewmodel.MineViewModel
+import com.sum.main.ui.system.adapter.ArticleAdapter
+import com.sum.network.error.ERROR
+import java.io.File
+
+/**
+ * @author mingyan.su
+ * @date   2023/3/3 8:22
+ * @desc   我的
+ */
+class MineFragment : BaseMvvmFragment<FragmentMineBinding, MineViewModel>(), OnRefreshListener,
+    OnLoadMoreListener {
+
+    private var mPage = 0
+    private lateinit var mHeadBinding: FragmentMineHeadBinding
+    private lateinit var mAdapter: ArticleAdapter
+
+    override fun initView(view: View, savedInstanceState: Bundle?) {
+        initRecyclerView()
+        initHeadView()
+        initListener()
+    }
+
+    override fun initData() {
+        val user = UserServiceProvider.getUserInfo()
+        setUserInfo(user)
+        UserServiceProvider.getUserLiveData().observe(this) {
+            setUserInfo(it)
+        }
+    }
+
+    override fun onFragmentVisible(isVisibleToUser: Boolean) {
+        LogUtil.e("isVisibleToUser:$isVisibleToUser")
+    }
+
+    /**
+     * 设置用户信息
+     */
+    private fun setUserInfo(user: User?) {
+        LogUtil.e("userdata:$user", tag = "smy")
+        if (UserServiceProvider.isLogin()) {
+            user?.let {
+                mHeadBinding.ivHead.loadFile(File(it.icon ?: ""))
+                if (!it.nickname.isNullOrEmpty()) {
+                    mHeadBinding.tvName.text = it.nickname
+                } else {
+                    mHeadBinding.tvName.text = it.username
+                }
+                mHeadBinding.tvDesc.text = it.signature
+            } ?: kotlin.run {
+
+            }
+        } else {
+            mHeadBinding.tvName.text = getStringFromResource(R.string.mine_not_login)
+            mHeadBinding.tvDesc.text = getStringFromResource(com.sum.common.R.string.login_know_more_android)
+        }
+    }
+
+    private fun initListener() {
+        mHeadBinding?.apply {
+            ivHead.onClick {
+                if (UserServiceProvider.isLogin()) {
+                    ARouter.getInstance().build(USER_ACTIVITY_INFO).navigation()
+                } else {
+                    LoginServiceProvider.login(requireContext())
+                }
+            }
+            ivSetting.onClick {
+                ARouter.getInstance().build(USER_ACTIVITY_SETTING).navigation()
+            }
+            tvVideo.onClick {
+
+            }
+            tvWorkTitle.onClick {
+
+            }
+            tvLikeTitle.onClick {
+                if (UserServiceProvider.isLogin()) {
+                    ARouter.getInstance().build(USER_ACTIVITY_COLLECTION).navigation()
+                } else {
+                    LoginServiceProvider.login(requireContext())
+                }
+            }
+            tvNavigation.onClick {
+                ARouter.getInstance().build(DEMO_ACTIVITY_NAVIGATION).navigation()
+            }
+            tvLifeCycle.onClick {
+                ARouter.getInstance().build(DEMO_ACTIVITY_LIFECYCLE).navigation()
+            }
+            tvDataBinging.onClick {
+
+            }
+            tvLivedata.onClick {
+                ARouter.getInstance().build(DEMO_ACTIVITY_LIVEDATA).navigation()
+            }
+            tvViewModel.onClick {
+                ARouter.getInstance().build(DEMO_ACTIVITY_VIEWMODEL).navigation()
+            }
+            tvPaging.onClick {
+
+            }
+            tvRoom.onClick {
+
+            }
+            tvHilt.onClick {
+
+            }
+        }
+
+    }
+
+    private fun initRecyclerView() {
+        mBinding?.refreshLayout?.apply {
+            autoRefresh()
+            setEnableRefresh(true)
+            setEnableLoadMore(true)
+            setOnRefreshListener(this@MineFragment)
+            setOnLoadMoreListener(this@MineFragment)
+            autoRefresh()
+        }
+        mAdapter = ArticleAdapter()
+        val dp12 = dpToPx(12)
+        mBinding?.recyclerView?.apply {
+            layoutManager = LinearLayoutManager(requireContext())
+            addItemDecoration(NormalItemDecoration().apply {
+                setBounds(left = dp12, top = dp12, right = dp12, bottom = dp12)
+                setLastBottom(true)
+                setFirstHeadMargin(true)
+            })
+            adapter = mAdapter
+        }
+        mAdapter.onItemClickListener = { _: View, position: Int ->
+            val item = mAdapter.getItem(position)
+            if (item != null && !item.link.isNullOrEmpty()) {
+                MainServiceProvider.toArticleDetail(
+                    context = requireContext(),
+                    url = item.link!!,
+                    title = item.title ?: ""
+                )
+            }
+        }
+        mAdapter.onItemCollectListener = { _: View, position: Int ->
+            if (LoginServiceProvider.isLogin()) {
+                setCollectView(position)
+            } else {
+                LoginServiceProvider.login(requireContext())
+            }
+        }
+    }
+
+    private fun initHeadView() {
+        mHeadBinding = FragmentMineHeadBinding.inflate(LayoutInflater.from(requireContext()))
+        mHeadBinding?.tvName?.text = getStringFromResource(R.string.mine_not_login)
+        mAdapter.addHeadView(mHeadBinding.root)
+    }
+
+    override fun onRefresh(refreshLayout: RefreshLayout) {
+        mPage = 0
+        getRecommendList()
+    }
+
+    /**
+     * 获取推荐列表数据
+     */
+    private fun getRecommendList() {
+        mViewModel.getRecommendList(count = mPage).observe(this) {
+            if (mPage == 0) {
+                mAdapter.setData(it)
+                if (it.isNullOrEmpty()) {
+                    mHeadBinding.tvRecommendTitle.gone()
+                } else {
+                    mHeadBinding.tvRecommendTitle.visible()
+                }
+                mBinding?.refreshLayout?.finishRefresh()
+            } else {
+                mAdapter.addAll(it)
+                mBinding?.refreshLayout?.finishLoadMore()
+            }
+        }
+    }
+
+    override fun onLoadMore(refreshLayout: RefreshLayout) {
+        mPage++
+        getRecommendList()
+    }
+
+    /**
+     * 收藏和取消收藏
+     * @param position
+     */
+    private fun setCollectView(position: Int) {
+        val data = mAdapter.getItem(position)
+        data?.let { item ->
+            showLoading()
+            val collect = item.collect ?: false
+            mViewModel.collectArticle(item.id, collect).observe(this) {
+                dismissLoading()
+                it?.let {
+                    val tipsRes =
+                        if (collect) com.sum.common.R.string.collect_cancel else com.sum.common.R.string.collect_success
+                    TipsToast.showSuccessTips(tipsRes)
+                    item.collect = !collect
+                    mAdapter.updateItem(position, item)
+                }
+
+                if (it == ERROR.UNLOGIN.code) {
+                    LoginServiceProvider.login(requireContext())
+                }
+            }
+        }
+    }
+}
